50a7bf3412ba6a119828cf3b608a731d3deddcf2
ultimecreation <naserbechata@gmail.com>
2024-11-16 19:44:20 +0100

    removed types marked as any

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -2,18 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { catchError, filter, find, map, tap } from 'rxjs/operators';
+import { OlympicInterface } from '../models/Olympic';
 
 @Injectable({
     providedIn: 'root',
 })
 export class OlympicService {
     private olympicUrl = './assets/mock/olympic.json';
-    private olympics$ = new BehaviorSubject<any>(undefined);
+    private olympics$ = new BehaviorSubject<OlympicInterface[] | undefined | null>(undefined);
 
     constructor(private http: HttpClient) { }
 
     loadInitialData() {
-        return this.http.get<any>(this.olympicUrl).pipe(
+        return this.http.get<OlympicInterface[]>(this.olympicUrl).pipe(
             tap((value) => this.olympics$.next(value)),
             catchError((error, caught) => {
                 // TODO: improve error handling
@@ -27,7 +28,7 @@ export class OlympicService {
 
     getOlympics() {
         // return this.olympics$.asObservable();
-        return this.http.get<any>(this.olympicUrl).pipe(
+        return this.http.get<OlympicInterface[]>(this.olympicUrl).pipe(
             tap((value) => this.olympics$.next(value)),
             catchError((error, caught) => {
                 // TODO: improve error handling
@@ -38,9 +39,9 @@ export class OlympicService {
             })
         );
     }
-    getOlympicById(id: string) {
-        return this.http.get<any>(this.olympicUrl).pipe(
-            map((value) => value.find((val: any) => val.id == id)),
+    getOlympicById(id: number) {
+        return this.http.get<OlympicInterface[]>(this.olympicUrl).pipe(
+            map((value) => value.find((val: OlympicInterface) => val.id === id)),
             catchError((error, caught) => {
                 // TODO: improve error handling
                 console.error(error);
@@ -52,8 +53,8 @@ export class OlympicService {
     }
 
     getOlympicByCountry(country: string) {
-        return this.http.get<any>(this.olympicUrl).pipe(
-            map((value) => value.find((val: any) => val.country == country)),
+        return this.http.get<OlympicInterface[]>(this.olympicUrl).pipe(
+            map((value) => value.find((val: OlympicInterface) => val.country == country)),
             catchError((error, caught) => {
                 // TODO: improve error handling
                 console.error(error);
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, EventEmitter, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ChartOptions } from 'chart.js';
-import { map, Observable, of } from 'rxjs';
+import { ChartEvent, ChartOptions } from 'chart.js';
+import { Observable, of } from 'rxjs';
+import { OlympicInterface } from 'src/app/core/models/Olympic';
+import { ParticipationInterface } from 'src/app/core/models/Participation';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -13,28 +15,27 @@ export class HomeComponent implements OnInit {
     private destroyRef = inject(DestroyRef)
     private router = inject(Router)
 
-    public olympics$: Observable<any> = of(null);
+    public olympics$: Observable<OlympicInterface[] | null> = of(null);
     public countriesCount: number = 0
     public joCount: number = 0
     // Pie
     public pieChartOptions: ChartOptions<'pie'> = { responsive: true };
-    public pieChartLabels: any[] = []
+    public pieChartLabels: string[] = []
     public pieChartDatasets: [{ data: number[] }] = [{ data: [0, 0, 0, 0, 0] }];
 
     constructor(private olympicService: OlympicService) { }
 
     ngOnInit(): void {
         const subscription = this.olympicService.getOlympics().subscribe({
-            next: (data: any[]) => {
+            next: (data: OlympicInterface[]) => {
                 this.countriesCount = data.length
-                console.log(data)
                 for (let item of data) {
                     const participationsLength = item.participations.length
                     if (this.joCount < participationsLength) this.joCount = participationsLength
 
                     this.pieChartLabels = [... this.pieChartLabels, item.country]
                     const totalMedalsCount = item.participations.reduce(
-                        (acc: any, current: any) => {
+                        (acc: number, current: ParticipationInterface) => {
                             return acc + current.medalsCount
                         }, 0)
                     this.pieChartDatasets[0].data.unshift(totalMedalsCount)
@@ -46,13 +47,17 @@ export class HomeComponent implements OnInit {
         this.destroyRef.onDestroy(() => subscription.unsubscribe())
     }
 
-    onChartClick = (e: any) => {
-        window.console.log('onChartClick', e);
-        if (e.active.length > 0) {
-            const country = this.pieChartLabels[e.active[0].index];
+    onChartClick($event: {
+        event?: ChartEvent;
+        active?: object[] | any;
+    }) {
+        console.log($event.active)
+        if ($event.active && $event.active.length > 0) {
+            const country = this.pieChartLabels[$event.active[0].index];
             const subscription = this.olympicService.getOlympicByCountry(country).subscribe({
-                next: (data: any) => {
-                    this.router.navigateByUrl(`details/${data.id}`)
+                next: (data: OlympicInterface | undefined) => {
+
+                    this.router.navigateByUrl(`details/${data?.id}`)
                 }
             })
             this.destroyRef.onDestroy(() => subscription.unsubscribe())
